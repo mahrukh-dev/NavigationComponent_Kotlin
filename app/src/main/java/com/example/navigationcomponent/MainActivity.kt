188619feec20dc97945897b91497f3e51207a842
MahRukh <mahrukhdev@gmail.com>
2023-08-09 13:13:32 +0500

    Navigation drawer implemented

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.navigationcomponent
 
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,20 +24,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = findNavController(R.id.fragmentContainerView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
         drawerLayout = findViewById(R.id.drawerLayout)
         val navigationView: NavigationView = findViewById(R.id.navigationView)
         navigationView.setupWithNavController(navController)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
 
+        listener = NavController.OnDestinationChangedListener{ controller, destination, arguments ->
+
+                if (destination.id == R.id.firstFragment) {
+                    supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.fragment_one_background)))
+                } else if (destination.id == R.id.secondFragment) {
+                    supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.fragment_two_background)))
+                }
+
+        }
+
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(listener)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragmentContainerView)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+//        return super.onSupportNavigateUp()
     }
 }
